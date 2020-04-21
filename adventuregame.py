ce0dfdcc3d5e28613a94a07eb898097d141c839a
tinbolw <tinbolw@gmail.com>
2020-04-21 10:40:16 -0700

    made it so if you died the game wouldnt instantly quit, and made world selection easier

--- adventuregame.py
+++ adventuregame.py
@@ -1,23 +1,16 @@
 world_selection = input("What world do you want to play? \n1.Hell World \n2.Grassy World \n3.Ice World\n")
 
-if world_selection == str("Hell World"):
+if world_selection == str(1):
     print("loading hellworld...")
     import hellworld as hw
-elif world_selection == str("Grassy World"):
+elif world_selection == str(2):
     print("loading grassy world...")
     import grassyworld as gw
-elif world_selection == str("Ice World"):
+elif world_selection == str(3):
     print("loading ice world...")
     import iceworld as iw
 else:
     print("Invalid selection")
     quit()
 
-end_game_screen = input("Type Quit to quit, type Stats to see your stats")
-if end_game_screen == str("Quit"):
-    quit()
-elif end_game_screen == str("Stats"):
-    print("Wip")
-else:
-    print("Invalid option")
-    quit()
+
--- hellworld.py
+++ hellworld.py
@@ -1,18 +1,84 @@
 user_name = input("What is your characters name? ")
 import random
+import time
+
+mob_killed = 0
+
+total_gold = 0
+
+
+def view_stats():
+    print("You killed " + str(mob_killed) + " mobs!")
+    print("You earned " + str(total_gold) + " gold!")
+
 
 health = 10
 money = 0
 inventory = [""]
 
+if health <= 0:
+    end_option = input("You died!\nType Stats to see your stats and Quit to quit")
+
+
+    def last_option():
+        if end_option == str("Stats"):
+            view_stats()
+        elif end_option == str("Quit"):
+            print("Goodbye.")
+            quit()
+        else:
+            print("Invalid option")
+            last_option()
+
+
 def run_option4():
     print("you have reached the end of the game! (for now")
+    time.sleep(10)
+    quit()
+
 
 def run_option4w():
     print("you have reached the end of the game! (for now")
+    time.sleep(10)
+    quit()
+
 
 def run_option4a():
-    print("you have reached the end of the game! (for now")
+    option4a = input("Choose an option:\n1. Try to find some useful materials nearby\n2. Do nothing\n3. Swim across "
+                     "the lava\n")
+    if option4a == str(1):
+        def run_option3a():
+            option3a = input(
+                "Choose an option:\n1. Attack the nearest creature\n2. Dig up the nearest plant\n3. Scout around "
+                "this area\n")
+            if option3a == str(1):
+                print(
+                    "You run up to the nearest pigman and punch him!\nHe oinks and stabs you with his sword.\nYou "
+                    "punch him "
+                    "another 19 times in quick sucession somehow and he dies.\n Luckily there are no other pigmen "
+                    "nearby to "
+                    "witness this event.")
+                health -= 2
+                mob_killed += 1
+                print("You got a gold sword!")
+                inventory.append("Gold Sword")
+                print("Your health is: " + str(health))
+                print("You have " + str(money) + " gold coins.")
+                run_option4()
+            elif option3a == str(2):
+                print(
+                    "You walk over to the nearest plant, a strange tree, and punch it multiple times until it breaks.")
+                print("You got Strange Wood!")
+                inventory.append("Strange Wood")
+                run_option4w()
+                if option4a == str(2):
+                    print("You do nothing.")
+                    run_option4a()
+                elif option4a == str(3):
+                    print("You leap into the lava and immediately start burning.")
+                    print("You died! Relaunch the game to play again")
+                    time.sleep(10)
+                    quit()
 
 
 def run_option3():
@@ -34,7 +100,9 @@ def run_option3():
         inventory.append("Strange Wood")
         run_option4w()
     elif option3 == str(3):
-        print("You scout around the area and find that there is a fortress a hundred blocks away from you.\n It is across the lava though...")
+        print(
+            "You scout around the area and find that there is a fortress a hundred blocks away from you.\n It is "
+            "across the lava though...")
         run_option4a()
     else:
         print("Invalid option")
@@ -52,6 +120,7 @@ def run_option2():
     elif option2 == str(2):
         print("You jump into the lava, and within seconds, you get burnt to a crisp.\nYou died! Relaunch the game to "
               "play again")
+        time.sleep(10)
         quit()
     elif option2 == str(3):
         print("You do nothing.")
@@ -75,10 +144,10 @@ def run_option1():
     elif option1 == str(2):
         print("You slam your fist into the ground and the ground shakes! The ground splits where you punched it and "
               "crumbles into the lava under it. You died! Relaunch the game to play again")
+        time.sleep(10)
         quit()
     elif option1 == str(3):
         print("You died! Relaunch the game to try again")
-        quit()
     else:
         print("Invalid option")
         run_option1()
